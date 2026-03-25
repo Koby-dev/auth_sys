afb2fe3dfa81a81ad193d0db3b6ebbc72be4b62b
Koby-dev <brianobichukwu@gmail.com>
2026-03-25 13:52:32 +0100

    Set temporary environment variables for Render deployment

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+# Temporary environment variables
+export SECRET_KEY='535^-06t^^$$_vt48xz++t%(5(j76w6)z32p+694wpiat=$-b$'
+export DEBUG=False
+export ALLOWED_HOSTS='auth-system-gz9p.onrender.com,127.0.0.1,localhost'
+
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
